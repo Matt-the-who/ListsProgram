65dad3ae1e1c1fd623ff34d0ed1e1f580adbec4b
Matt-the-who <matthew.ho@ocsbstudent.ca>
2015-11-18 13:03:14 -0500

    Commit

---
+++ ListsProgram/ListsProgram.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ListsProgram
+{
+    partial class frmListsProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.nudNumberToAdd = new System.Windows.Forms.NumericUpDown();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.mnuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberToAdd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mnuStrip.TabIndex = 0;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(62, 267);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(85, 13);
+            this.lblAverage.TabIndex = 1;
+            this.lblAverage.Text = "The Average is: ";
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(167, 68);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(77, 173);
+            this.lstBox.TabIndex = 2;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(25, 125);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Average";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // nudNumberToAdd
+            // 
+            this.nudNumberToAdd.Location = new System.Drawing.Point(14, 90);
+            this.nudNumberToAdd.Name = "nudNumberToAdd";
+            this.nudNumberToAdd.Size = new System.Drawing.Size(100, 20);
+            this.nudNumberToAdd.TabIndex = 5;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(25, 155);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmListsProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 301);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.nudNumberToAdd);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmListsProgram";
+            this.Text = "Lists Program";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberToAdd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.NumericUpDown nudNumberToAdd;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ ListsProgram/ListsProgram.cs
@@ -0,0 +1,76 @@
+﻿/*
+Created by Matthew Ho
+Created on 11-16-15
+Created for ICS3U
+Assignment #5-04
+This program uses a list to calculate the average of a set of numbers.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListsProgram
+{
+    public partial class frmListsProgram : Form
+    {
+        //Global
+        double numberOfmarks = 0;
+        double totalValue = 0;
+        double number = 0;
+        double average = 0;
+
+        List<double> listOfNumbers = new List<double>();
+
+        public double Averaging(double total, double numberOfNumbers)
+        {//This averages numbers.
+            double average = 0;
+
+            average = total / numberOfNumbers;
+
+            return average;
+        }
+
+        public frmListsProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            number = Convert.ToInt32(this.nudNumberToAdd.Value);
+
+            listOfNumbers.Add(number);
+
+            this.lstBox.Items.Add(number);
+
+            totalValue = listOfNumbers.Sum();
+
+            numberOfmarks = numberOfmarks + 1;
+
+            average = Averaging(totalValue, numberOfmarks);
+
+            this.lblAverage.Text = "The Average is: " + average;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listOfNumbers.Clear();
+
+            this.lstBox.Items.Clear();
+
+            this.lblAverage.Text = "The Average is: ";
+        }
+    }
+}
